33d5fdfd3d45987babe18d78872141df185cc5c6
halldorpalmi <48418293+halldorpalmi@users.noreply.github.com>
2025-08-26 02:41:26 +0000

    Forsíðu breytingar

--- github-pages-site/assets/js/scripts.js
+++ github-pages-site/assets/js/scripts.js
@@ -1,1 +1,23 @@
 // This file is intentionally left blank.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const footer = document.querySelector("footer p");
+    const currentDate = new Date().toLocaleDateString("is-IS", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+    footer.textContent = `Dags: ${currentDate}`;
+});
+
+
+document.getElementById("colorButton").addEventListener("click", function () {
+    console.log("Button clicked!");
+    const gradients = [
+        "linear-gradient(to right, #EB3349, #F45C43)",
+        "linear-gradient(to right, #4CB8C4, #3CD3AD)",
+        "linear-gradient(to right, #DA22FF, #9733EE)",
+        "linear-gradient(to right, #cc2b5e, #753a88)"
+    ];
+    document.body.style.background = gradients[Math.floor(Math.random() * gradients.length)];
+});
